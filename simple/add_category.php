5584aa4f6695d52a1b11862e1fbe706a2d9cd7d8
Mohamed Ifah <ifaa.x.h@gmail.com>
2015-02-05 22:31:11 +0500

    PHP Tutorials Creating a Blog
    Creating a blog, from start to finish, with a MySQL database. It's
    recommended you watch the MySQL tutorial series before attempting this
    tutorial, unless you're confident in MySQL.

---
+++ simple/add_category.php
@@ -0,0 +1,45 @@
+<?php
+include_once('resources/init.php');
+if(isset($_POST['name'])){
+	if(!empty(trim($_POST['name']))){
+		$name = trim($_POST['name']);
+		if(category_exists($name)){
+			$error = 'That category already exists.';
+		}else if(strlen($name)>24){
+			$error = 'Category name can only be up to 24 characters.';
+		}else{
+			add_category($name);
+
+			header('Location: add_post.php');
+			die();
+		}
+	}else{
+		$error = 'You must submit a category name.';
+	}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Add a Category</title>
+</head>
+<body>
+	<h1> Add a Category </h1>
+	<?php
+		if(isset($error)){
+			echo "<p> $error </p>\n";
+		}
+	?>
+	<form action="" method="post">
+		<div>
+			<label for="name"> Name </label>
+			<input type="text" name="name" value="">
+		</div>
+		<div>
+			<input type="submit" value="Add Category">
+		</div>
+	</form>
+</body>
+</html>
---
+++ simple/resources/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db_host'] = 'localhost';
+$config['db_user'] = 'ifah';
+$config['db_pass'] = 'password';
+$config['db_name'] = 'blog';
+
+foreach($config as $k=>$v){
+	define(strtoupper($k), $v);
+}
+
+?>
